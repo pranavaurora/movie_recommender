ef37d04f6664b4fe40f510a614358392119fb0c4
arno756 <arno756@users.noreply.github.com>
2023-04-14 22:36:09 -0700

    Changes

--- movie-recommender-flask/venv/main.py
+++ movie-recommender-flask/venv/main.py
@@ -14,7 +14,7 @@ def init_connection():
 
 app = Flask(__name__)
 app.config["MYSQL"] = {
-    "host": "svc-a3f7f2ba-378c-40ab-be2e-9fa993172504-dml.aws-virginia-5.svc.singlestore.com",
+    "host": "svc-a3f7f2ba-378c-40ab-863e-3c53ea0efe9b-dml.aws-virginia-5.svc.singlestore.com",
     "user": "admin",
     "password": "Test1234",
     "database": "movie_recommender"
@@ -63,58 +63,77 @@ def movie_recommendation():
         selected_movies = request.json['selected_movies']
         # Use the titles to generate movie recommendations
         conn = init_connection()
-        temp_table_name = "temp_table_" + str(uuid.uuid4()).replace("-", "")
-        with conn.cursor() as cursor:
-            # Create the temporary table with a unique name
-            cursor.execute(f"""
-                CREATE TEMPORARY TABLE {temp_table_name} (
-                    title VARCHAR(255)
-                );
-            """)
+        print(selected_movies)
+        user_session = "user_" + str(uuid.uuid4()).replace("-", "")
+        values_str = ', '.join(map(lambda movie: f"('{movie}')", selected_movies))
+        print(values_str)
 
-            # Insert the data from the selected_movies list in Python
-            for movie in selected_movies:
-                cursor.execute(
-                    "INSERT INTO temp_table_name (title) VALUES (%s)",
-                    (movie["title"])
-                )
-            # Commit the changes to the database
-            conn.commit()
-            
-            # Run the query you provided
-            cursor.execute("""
-                With table_match as (
-                    select m.title, m.movieId, m.vector from {temp_table_name} as t
-                    inner join movie_with_tags_with_vectors m
+        # define the SQL statement to insert a row into the user_choice table
+        sql_insert = "INSERT INTO user_choice (userid, title, ts) VALUES (%s, %s, now())"
+
+        # define a list of tuples, where each tuple contains the values for a single row to be inserted
+        movie_data = []
+        for movie in selected_movies:
+            movie_data.append((user_session, movie))
+
+        # execute the insert statement for all the rows in one go
+        cursor = conn.cursor()
+        cursor.executemany(sql_insert, movie_data)
+        conn.commit()
+
+        print(conn)
+        print(cursor)
+        
+        # Run the query you provided
+        cursor.execute(f"""
+                with table_match as (
+                SELECT m.title, m.movieId, m.vector
+                FROM user_choice t
+                INNER JOIN movie_with_tags_with_vectors m on m.title = t.title
+                where userid=%s
                 ),
-                movie_pairs AS (
-                    SELECT m1.movieId AS movieId1, m1.title as title1, m2.movieId AS movieId2, m2.title as title2, DOT_PRODUCT(m1.vector, m2.vector) AS similarity 
-                    FROM table_match m1 
-                    CROSS JOIN movie_with_tags_with_vectors m2
-                    WHERE m1.movieId != m2.movieId),
-                    movie_match as ( 
-                        SELECT movieId1,title1, movieId2,title2, similarity 
-                        FROM movie_pairs 
-                        WHERE similarity > 0.4 
-                        order by similarity desc), 
-                            distinct_count as ( 
-                                select distinct movieId2, title2 as Title, round(avg(similarity),2) as Rating_Match from movie_match 
-                                group by movieId2,title2 
-                                order by Rating_Match desc)
-                ;
-            """)
-            
-            # Fetch the result rows
-            output_list = cursor.fetchall()
-            
+            movie_pairs AS (
+                SELECT m1.movieId AS movieId1, m1.title as title1, m2.movieId AS movieId2, m2.title as title2, DOT_PRODUCT(m1.vector, m2.vector) AS similarity 
+                FROM table_match m1 
+                CROSS JOIN movie_with_tags_with_vectors m2
+                WHERE m1.movieId != m2.movieId),
+                movie_match as ( 
+                    SELECT movieId1,title1, movieId2,title2, similarity 
+                    FROM movie_pairs 
+                    WHERE similarity > 0.4 
+                    order by similarity desc), 
+                        distinct_count as ( 
+                            select distinct movieId2, title2 as Title, round(avg(similarity),2) as Rating_Match from movie_match 
+                            group by movieId2,title 
+                            order by Rating_Match desc)
+                            SELECT Title, Rating_Match FROM distinct_count
+                            order by Rating_Match desc
+                            limit 5;
+        """, (user_session,))
+
+        # Fetch the result rows
+        
+        rows = cursor.fetchall()
+
+        results = []
+        for row in rows:
+            result = {}
+            result['title'] = row[0]
+            result['rating_match'] = row[1]
+            results.append(result)
+
+        # now you can use the results list as needed
+        print(results)
+
     finally:
-        if cursor:
+        if cursor is not None:
             cursor.close()
-        if conn:
+        if conn is not None:
             conn.close()
-    
-    df = pd.DataFrame(output_list, columns=["Title", "Rating_Match"])
-    return render_template("index.html", data=df.to_html(classes="table table-striped table-hover"))
+
+    df = pd.DataFrame(results, columns=["title", "rating_match"])
+    print(df)
+    #return render_template("index.html", data=df.to_html(classes="table table-striped table-hover"))
 
 #NONE OF THE STUFF BELOW IS IN USE ... We will need a new route for when the submit button is hit....
 
